175def06343dc80c7c2e6ce9661309c35d478be0
Winifred Lu <winnie.lu@comcast.net>
2022-01-13 14:52:16 -0500

    renders three error messages for zero inputs and submit button clicked

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -26,7 +26,11 @@ test('renders ONE error message if user enters less then 5 characters into first
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
-    
+    render(<ContactForm />);
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+    const errors = await screen.getAllByTestId('error');
+    expect(errors.length).toBe(3);
 });
 
 test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {

789855d5bac5e73f291a85f65100fefd5ace1ce0
Winifred Lu <winnie.lu@comcast.net>
2022-01-13 14:36:42 -0500

    contact form header render test complete

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -10,7 +10,9 @@ test('renders without errors', ()=>{
 });
 
 test('renders the contact form header', ()=> {
-    
+    render(<ContactForm />);
+    const header = screen.queryByText(/Contact Form/i);
+    expect(header).toBeInTheDocument();
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
